fe389488465820cd58beb2dbf609ac50425f852e
Daniele206 <danieledeieso2004@gmail.com>
2024-02-21 18:37:39 +0100

    [add] Milestone 2

--- js/script.js
+++ js/script.js
@@ -10,6 +10,13 @@ const selectAge = document.querySelector('.select-age');
 
 // output
 const tiket = document.querySelector('.tiket')
+const tiketContent = document.querySelector('.tiket-content')
+const myForm = document.querySelector('.my-form')
+const myName = document.querySelector('.my-name')
+const myDiscount = document.querySelector('.my-discount')
+const myNVagon = document.querySelector('.my-n-vagon')
+const myNTiket = document.querySelector('.my-n-tiket')
+const myTiketPrice = document.querySelector('.my-tiket-price')
 
 // buttons
 const btnCreate = document.querySelector('.btn-create')
@@ -27,20 +34,57 @@ btnCreate.addEventListener('click',function(event){
   if(selectAge.value === 'under18'){
     tiketPrice = tiketPrice - (tiketPrice * discountunder18 / 100)
     console.log(tiketPrice);
+    myDiscount.innerHTML = `
+    <span class="fw-bold">Offerta</span>
+    <p>Under18</p>
+    `;
   }else if(selectAge.value === 'over65'){
     tiketPrice = tiketPrice - (tiketPrice * discountover65 / 100)
     console.log(tiketPrice);
+    myDiscount.innerHTML = `
+    <span class="fw-bold">Offerta</span>
+    <p>Over65</p>
+    `;
+  }else{
+    myDiscount.innerHTML = `
+    <span class="fw-bold">Offerta</span>
+    <p>Standard</p>
+    `;
   }
 
   tiketPrice = tiketPrice.toFixed(2)
 
-  tiket.innerHTML = `
-  <p> Ciao ${inputName.value} il tuo bilgietto per fare ${inputKm.value}Km costa ${tiketPrice}&euro;</p>
+  console.log(tiketPrice);
+
+  myName.innerHTML = `
+  <span class="fw-bold fs-5">NOME PASSEGGERO</span>
+  <p>${inputName.value}</p>
   `
+  myNVagon.innerHTML = `
+  <span class="fw-bold">Carrozza</span>
+  <p>4</p>`;
+
+  myNTiket.innerHTML = `
+  <span class="fw-bold">Biglietto n:</span>
+  <p>4545</p>`;
+
+  myTiketPrice.innerHTML = `
+  <span class="fw-bold">Costo Biglietto</span>
+  <p>${tiketPrice}&euro;</p>`;
+
+  tiket.classList.remove('d-none')
+
+  myForm.classList.add('mb-5')
 });
 
 btnCancel.addEventListener('click',function(){
   inputName.value = '';
   inputKm.value = '';
   selectAge.value = 'Sono:';
+
+  tiket.innerHTML = `  `;
+
+  tiket.classList.add('d-none')
+
+  myForm.classList.remove('mb-5')
 });
